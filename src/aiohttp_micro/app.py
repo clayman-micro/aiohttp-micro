cc61eb952e71e85efe37b9caf6ab8da773a4da70
Kirill Sumorokov <sumorokov.k@gmail.com>
2021-03-25 23:34:45 +0500

    Resolve flake8 issue

--- src/aiohttp_micro/app.py
+++ src/aiohttp_micro/app.py
@@ -8,27 +8,6 @@ def init(app_name: str, cfg: AppConfig) -> web.Application:
 
     setup_micro(app, app_name, cfg, package_name="aiohttp_micro")
 
-    app["spec"] = APISpec(
-        title="Micro API",
-        version=app["distribution"].version,
-        openapi_version="3.0.2",
-        info={"description": "Micro service API"},
-        plugins=[MarshmallowPlugin()],
-    )
-
-    app.router.add_get("/api/spec.json", openapi.spec, name="api.spec")
-
-    for route in app.router.routes():
-        if route.method.lower() == "head":
-            continue
-
-        if hasattr(route.handler, "spec") and route.resource:
-            operation_spec = route.handler.spec["operation"]  # type: ignore
-            app["spec"].path(
-                path=route.resource.canonical,
-                operations={route.method.lower(): operation_spec},
-            )
-
     app["logger"].info("Initialize application")
 
     return app
